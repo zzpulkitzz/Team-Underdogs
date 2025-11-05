94bfc1a3fdf584cbac613ed70d5075a865b4537a
zzpulkitzz <gpulkitgupta72@gmail.com>
2025-11-06 00:31:26 +0530

    auth controller added

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const db = require('./models');
 const authRoutes = require('./routes/auth');
-
+console.log("d,",authRoutes)
 const app = express();
 app.use(express.json());
-
+app.get("/health",(req,res)=>{
+    res.send("ok")
+})
 app.use('/api/auth', authRoutes);
 
 db.sequelize.sync().then(() => {
--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,16 @@
 const { Sequelize } = require('sequelize');
 const config = require('../config').development;
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-  host: config.host,
-  dialect: config.dialect,
+const sequelize = new Sequelize("postgresql://postgres.cfcskwbthvaucoqvasog:cHRBzk4isT0kcOQN@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres", {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Supabase requires SSL
+    },
+  },
+  logging: false, // optional, turns off SQL logs
 });
 
 const db = {};
